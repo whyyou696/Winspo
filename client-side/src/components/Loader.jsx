8907276b104157ae9ffcead2d9d08137ab54b80c
whyyou696 <whyyousick696@gmail.com>
2024-01-17 07:23:25 +0700

    feat:songcard

--- client-side/src/components/Loader.jsx
+++ client-side/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { loader } from '../assets';
 const Loader = () => (
   <div className="w-full h-full flex items-center justify-center bg-primary">
     <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
-    <h1 className="text-white text-2xl font-bold">Loading...</h1>
+    <h1 className="text-white text-6xl font-bold">Loading...</h1>
   </div>
 );
 
--- client-side/src/pages/Discover.jsx
+++ client-side/src/pages/Discover.jsx
@@ -1,7 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+// CAKE = {
+    
+//     SLICE: MUSIC PLAYER FUNCTIONALITY
+//     SLICE: MUSIC PLAYER FUNCTIONALITY
+// }
 const Discover = () => {
+    const dispatch = useDispatch();
+    // const {} = useSelector((CAKE) => CAKE.VANILLA )
+
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
@@ -13,11 +25,11 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-left">Discover <br/>Genre : "{genreTitle}"</h2>
+        <h2 className="font-bold text-3xl text-left text-[#ff5151]">Discover <br/>Genre : "{genreTitle}"</h2>
         <select
           onChange={() => {}}
           value=""
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          className="bg-[#ff5151] text-white p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
             <option key={genre.value} value={genre.value}>
