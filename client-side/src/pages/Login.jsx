97ec6f213fda838ccf1a84369de9b97a5c04d0d4
whyyou696 <whyyousick696@gmail.com>
2024-01-19 03:58:57 +0700

    fix:googleid

--- client-side/src/pages/Login.jsx
+++ client-side/src/pages/Login.jsx
@@ -77,7 +77,7 @@ export default function Login() {
 
   useEffect(() => {
     google.accounts.id.initialize({
-      client_id: "261169357803-keuu9oq6gov5s5b2ddvkmaia56h4v3rc.apps.googleusercontent.com",
+      client_id: import.meta.env.VITE_CLIENT_GOOGLE_ID,
       callback: handleCredentialResponse,
     });
     google.accounts.id.renderButton(
